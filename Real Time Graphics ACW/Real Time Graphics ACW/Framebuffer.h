8cba19bfe2403a43dc74c925a2d465484507766e
Ben Marshall <ben321mars@gmail.com>
2019-11-20 12:34:29 +0000

    Started work on the framebuffer

---
+++ Real Time Graphics ACW/Real Time Graphics ACW/Framebuffer.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <d3d11.h>
+
+class Framebuffer
+{
+	ID3D11Texture2D * mTexture;
+	ID3D11RenderTargetView * mTextureTargetView;
+	ID3D11ShaderResourceView * mTextureResourceView;
+	
+public:
+	Framebuffer(const bool & pColour, const bool & pDepth);
+	~Framebuffer();
+
+	Framebuffer(const Framebuffer & pFramebuffer) = delete;
+	Framebuffer & operator= (const Framebuffer & pFramebuffer) = delete;
+};
+

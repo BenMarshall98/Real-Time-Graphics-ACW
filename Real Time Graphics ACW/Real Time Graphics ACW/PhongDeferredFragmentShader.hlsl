413dbedcd44322acb61b0889dd6797cdd4f5d54a
Ben Marshall <ben321mars@gmail.com>
2019-12-29 18:39:17 +0000

    Deferred shader writen for phong technique

---
+++ Real Time Graphics ACW/Real Time Graphics ACW/PhongDeferredFragmentShader.hlsl
@@ -0,0 +1,37 @@
+cbuffer materialBuffer : register(b1)
+{
+    float4 MaterialAmbient;
+    float4 MaterialDiffuse;
+    float4 MaterialSpecular;
+    float MaterialShininess;
+}
+
+struct VS_OUTPUT
+{
+    float4 Pos : SV_POSITION;
+    float4 FragmentPos : POSITION0;
+    float4 Normal : NORMAL0;
+    float4 LightFragmentPos : POSITION1;
+};
+
+struct PS_OUTPUT
+{
+    float4 Pos : SV_Target0;
+    float4 Normal : SV_Target1;
+    float4 Ambient : SV_Target2;
+    float4 Diffuse : SV_Target3;
+    float4 Specular : SV_Target4;
+};
+
+PS_OUTPUT main(VS_OUTPUT input)
+{
+    PS_OUTPUT output = (PS_OUTPUT) 0;
+    
+    output.Pos = float4(input.FragmentPos.xyz, 2.0f);
+    output.Normal = input.Normal;
+    output.Ambient = MaterialAmbient;
+    output.Diffuse = MaterialDiffuse;
+    output.Specular = float4(MaterialSpecular.xyz, MaterialShininess);
+    
+    return output;
+}
